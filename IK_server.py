b4ea7fc4f2092f8f71960c906c217d17a0ba6cf1
Nick Z <34175716+nicholaszk@users.noreply.github.com>
2018-03-19 21:29:11 -0500

    Update IK_server.py

--- IK_server.py
+++ IK_server.py
@@ -38,13 +38,13 @@ def handle_calculate_IK(req):
         q1, q2, q3, q4 ,q5, q6, q7 = symbols('q1:8')
 
 	    # Create Modified DH parameters
-        DH_Table = { alpha0:      0,  a0:      0,  d1:  0.75,  q1:          q1,
-		         alpha1: -pi/2.,  a1:   0.35,  d2:     0,  q2: -pi/2. + q2,
-			     alpha2:      0,  a2:   1.25,  d3:     0,  q3:          q3,
-			  	 alpha3: -pi/2.,  a3:  -.054,  d4:   1.5,  q4:          q4,
-			   	 alpha4:  pi/2.,  a4:      0,  d5:     0,  q5:          q5,
-		    	 alpha5: -pi/2.,  a5:      0,  d6:     0,  q6:          q6,
-		    	 alpha6:      0,  a6:      0,  d7: 0.303,  q7:           0}
+        DH_Table = { alpha0:       0,  a0:      0,  d1:  0.75,  q1:          q1,
+		     alpha1:  -pi/2.,  a1:   0.35,  d2:     0,  q2: -pi/2. + q2,
+	             alpha2:       0,  a2:   1.25,  d3:     0,  q3:          q3,
+		     alpha3:  -pi/2.,  a3:  -.054,  d4:   1.5,  q4:          q4,
+		     alpha4:   pi/2.,  a4:      0,  d5:     0,  q5:          q5,
+		     alpha5:  -pi/2.,  a5:      0,  d6:     0,  q6:          q6,
+		     alpha6:       0,  a6:      0,  d7: 0.303,  q7:           0}
 
 	    # Define Modified DH Transformation matrix
         def TF_Matrix(alpha, a, d, q):
